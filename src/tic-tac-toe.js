d46c7430e31b3056ef7755c413a1ce34472bc4cd
fainz777 <fainz777@gmail.com>
2019-09-25 09:56:05 +0300

    tic-tac-toe/feature: solution [in progress]

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,23 @@
 class TicTacToe {
     constructor() {
-
+        this.isX = true;
+        this.currentPlayerSymbol = 'x';
+        this.board = [[null, null, null], [null, null, null], [null, null, null]]
+        //this.board = Array(3).fill(Array(3).fill(null));
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        const value = this.board[rowIndex][columnIndex];
 
+        if (!value) {
+            this.board[rowIndex][columnIndex] = this.currentPlayerSymbol;
+            this.currentPlayerSymbol = this.isX ? 'o' : 'x';
+            !this.isX;
+        }
     }
 
     isFinished() {
@@ -20,7 +29,7 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+        return JSON.stringify(this.board).indexOf('null') === -1;
     }
 
     isDraw() {
@@ -28,7 +37,24 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
+        return this.board[rowIndex][colIndex];
+    }
 
+    hasWinner() {
+        const winPositions = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[0, 2], [1, 1], [2, 0]];
+        ];
+
+        winPositions.forEach(position => {
+            
+        });
     }
 }
 
